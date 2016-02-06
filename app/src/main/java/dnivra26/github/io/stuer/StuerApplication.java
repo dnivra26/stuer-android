19e27a036ac66f824f677967a0a9e7b115991b61
dnivra26 <dnivra26@gmail.com>
2016-02-06 23:17:48 +0530

    Add default parse push notification receivers

--- app/src/main/java/dnivra26/github/io/stuer/StuerApplication.java
+++ app/src/main/java/dnivra26/github/io/stuer/StuerApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.parse.Parse;
 import com.parse.ParseACL;
+import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
@@ -16,6 +17,7 @@ public class StuerApplication extends Application {
         super.onCreate();
 
         Parse.initialize(this, "JvudinD8Q7F5SINjpZyYvU83b8AmjJQ7V3T86vmT", "ly2vSAIs9xOShFzqN8qEug48AbzxrjAx3srR1Gg9");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
 
         ParseUser.enableAutomaticUser();
         ParseACL defaultACL = new ParseACL();
